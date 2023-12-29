52a17edd9b5aa23ffd0f3eaee818cd7595062af4
Denys <deibukdenyscloud@gmail.com>
2023-12-29 19:38:42 +0200

    Redirect from root to schema list endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.views.generic.base import RedirectView
 
 from user.views import LogoutConfirmation, UserRegisterView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="schema_list")),
+    path("", include("app.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/register/", UserRegisterView.as_view(), name="register"),
@@ -12,4 +16,10 @@ urlpatterns = [
         LogoutConfirmation.as_view(),
         name="confirm_logout",
     ),
-]
+] + (
+    [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+    if settings.DEBUG
+    else []
+)
